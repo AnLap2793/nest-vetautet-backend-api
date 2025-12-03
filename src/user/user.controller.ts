81d95f3a2a099883c20cdbbc325f721feecd582b
leanlap <leanlap2k3@gmail.com>
2025-12-03 17:24:10 +0700

    feat: add file upload functionality with multer and express

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,15 +6,44 @@ import {
   Patch,
   Param,
   Delete,
+  UploadedFile,
+  UseInterceptors,
+  BadRequestException,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { RegisterUserDto } from 'src/user/dto/register-user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { storage } from 'src/user/oss';
+import * as path from 'path';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'uploads/avatar',
+      storage: storage,
+      limits: {
+        fileSize: 1024 * 1024 * 3,
+      },
+      fileFilter(req, file, callback) {
+        const extName = path.extname(file.originalname);
+        if (['.jpg', '.png', 'gif'].includes(extName)) {
+          callback(null, true);
+        } else {
+          callback(new BadRequestException('Upload file Error'), false);
+        }
+      },
+    }),
+  )
+  @Post('upload/avt')
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log('upload file -> ', file.path);
+    return file.path;
+  }
 
   @Post('new')
   register(@Body() registerUserDto: RegisterUserDto) {
